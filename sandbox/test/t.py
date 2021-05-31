0f6f3693894e5b5e288a1978de44cd8945203b17
dos09 <fake@fake.com>
2021-06-01 00:20:29 +0300

    test

--- sandbox/test/t.py
+++ sandbox/test/t.py
@@ -1,23 +1,6 @@
-
+def run():
+    print('ok')
+    
+ 
 if __name__ == '__main__':
-    arr = '20 7 8 2 5'.split()
-    index = 0
-    arr = sorted(
-        [(index, int(value)) 
-        for index, value in enumerate(arr)],
-        key=lambda x: x[1],
-        reverse=True
-    )
-    res = 999999999999
-    arr_len = len(arr)
-    for i in range(arr_len):
-        index, value = arr[i]
-        for j in range(i + 1, arr_len):
-            inner_index, inner_value = arr[j]
-            if index < inner_index:
-                r = value - inner_value
-                if r > 0 and r < res:
-                    res = r
-
-                break
-    print(res)
+    run()
