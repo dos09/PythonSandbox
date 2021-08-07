2e679737a223526932ac16e55ca7a9a4d7c14a79
dos09 <fake@fake.com>
2021-08-07 11:55:30 +0300

    show defaults for searching_horror_movies

--- sandbox/single_scripts/searching_for_horror_movies.py
+++ sandbox/single_scripts/searching_for_horror_movies.py
@@ -33,7 +33,9 @@ class MyHTMLParser(HTMLParser):
 
 
 def read_cli():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument('--file-name', required=True,
                         help='File with pasted webpage(s) source code')
     parser.add_argument('--movie-type', default='Ужас',
